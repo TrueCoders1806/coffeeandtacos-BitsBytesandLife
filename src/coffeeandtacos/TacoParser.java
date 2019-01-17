6e18ede0d014e1ba97bb7a94f681255b9df6c93a
BK <brian_j_king@me.com>
2019-01-17 10:24:06 -0600

    First Commit

--- src/coffeeandtacos/TacoParser.java
+++ src/coffeeandtacos/TacoParser.java
@@ -8,8 +8,38 @@ public class TacoParser {
     public Trackable Parse(String line) {
         
         logger.logInfo("Begin Parsing");
+        if(line == null)
+            {
+                logger.logWarning("Line was null");
+                return null;
+            }
+        // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+        var cells = line.split(",");
         
         // Do not fail if one record parsing fails, return null
-        return null; //TODO Implement
+        // If your array.Length is less than 3, something went wrong
+        if (cells.length < 3 || cells.length > 3)
+        {
+            // Log that and return null
+            logger.logWarning("The length was the array less than 3");
+            return null;
+        }
+        
+        var tacoBellLocation = new Trackable();
+        var tacoBellLocationPoint = new Point();
+        
+        // grab the latitude from your array at index 0
+        // tacoBellLocationPoint.latitude = convert.ToDouble(cells[0]);
+        tacoBellLocationPoint.setLatitude(Double.parseDouble(cells[0]));
+        tacoBellLocationPoint.setLongitude(Double.parseDouble(cells[1]));
+        
+        var name = cells[2].trim();
+        
+        tacoBellLocation.setName(name);
+        tacoBellLocation.setLocation(tacoBellLocationPoint);
+       
+            
+        // Do not fail if one record parsing fails, return null
+        return tacoBellLocation; //TODO Implement
     }
 }
