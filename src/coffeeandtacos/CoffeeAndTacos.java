ef126f559eed45cc29103f98f49280e23e532fef
BK <brian_j_king@me.com>
2019-01-28 20:32:44 -0600

    cleaning up the code

--- src/coffeeandtacos/CoffeeAndTacos.java
+++ src/coffeeandtacos/CoffeeAndTacos.java
@@ -25,33 +25,29 @@ public class CoffeeAndTacos {
         logger.logInfo("File Parsed");
         // TODO:  Find the two Taco Bells in Alabama that are the furthest from one another.
         // HINT:  You'll need two nested forloops
-        
         Trackable locA = null;
         Trackable locB = null;
-            //Trackable locA = null;
-            //Trackable locB = null;
-            double distance = 0;
+        
+        double distance = 0;
             
-            for (Trackable strartingTacoBellLocation : locations){
-                //var origin = locA.getLocation().getLatitude();
-                // Creating a new Coordinate with locations locA lat and long
-                //var origin = new GeoCoordinate(locations[i].Location.Latitude, locations[i].Location.Longitude);
-                var origin = new GeoCoordinate(strartingTacoBellLocation.getLocation().getLatitude(),
-                                               strartingTacoBellLocation.getLocation().getLongitude());
-                for (Trackable endindTacoBellLocation : locations){
-                    var destination = new GeoCoordinate(endindTacoBellLocation.getLocation().getLatitude(),
-                                               endindTacoBellLocation.getLocation().getLongitude());
-                    var currentDistance = origin.getDistanceTo(destination);
-                    if (currentDistance > distance)
-                    {
-                        distance = currentDistance;
-                        locA = strartingTacoBellLocation;
-                        locB = endindTacoBellLocation;
+        for (Trackable strartingTacoBellLocation : locations){
+            
+            //Creating a new Coordinate with locations locA lat and long
+            var origin = new GeoCoordinate(strartingTacoBellLocation.getLocation().getLatitude(),
+                                           strartingTacoBellLocation.getLocation().getLongitude());
+            for (Trackable endindTacoBellLocation : locations){
+                 var destination = new GeoCoordinate(endindTacoBellLocation.getLocation().getLatitude(),
+                                                     endindTacoBellLocation.getLocation().getLongitude());
+                 var currentDistance = origin.getDistanceTo(destination);
+                 if (currentDistance > distance){
+                     distance = currentDistance;
+                     locA = strartingTacoBellLocation;
+                     locB = endindTacoBellLocation;
                     }
                 }
-                //logger.logInfo("File Parsed");
-                logger.logInfo("\nStarting Taco Bell:" + locA.getName() + "\nEnding Taco Bell:" + locB.getName() +  
-                               "\nDistance:" + distance + " miles.");
-            }
+            
+            logger.logInfo("\nStarting Taco Bell:" + locA.getName() + "\nEnding Taco Bell:" + locB.getName() +  
+                           "\nDistance:" + distance + " miles.");
+        }
     }  
 }
--- src/coffeeandtacos/TacoParser.java
+++ src/coffeeandtacos/TacoParser.java
@@ -7,18 +7,16 @@ public class TacoParser {
     
     public Trackable Parse(String line) {
         
-        if(line == null)
-            {
-                logger.logWarning("Line was null");
-                return null;
-            }
+        if(line == null){
+            logger.logWarning("Line was null");
+            return null;
+        }
         // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
         var cells = line.split(",");
         
         // Do not fail if one record parsing fails, return null
         // If your array.Length is less than 3, something went wrong
-        if (cells.length > 3 ) 
-        {
+        if (cells.length > 3 ){
             // Log that and return null
             logger.logWarning("The length was the array less than 3");
             return null;
@@ -29,33 +27,27 @@ public class TacoParser {
         
        //grab the latitude from your array at index 0
        //validate the latitude if not log the error
-        try
-        {    
-           tacoBellLocationPoint.setLatitude(Double.parseDouble(cells[0]));
+        try {    
+            tacoBellLocationPoint.setLatitude(Double.parseDouble(cells[0]));
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             logger.logWarning("Not a Number", e);
             return null;
         }
-        if (tacoBellLocationPoint.getLatitude() < -90 || tacoBellLocationPoint.getLatitude() > 90)
-        {
+        if (tacoBellLocationPoint.getLatitude() < -90 || tacoBellLocationPoint.getLatitude() > 90) {
             logger.logWarning("Not a Valid Latitude.");
             return null;
         }
         // grab the longitude from your array at index 1
         // validate the longitude if not log the error
-        try
-        {
+        try {
           tacoBellLocationPoint.setLongitude(Double.parseDouble(cells[1]));
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
            logger.logWarning("Not a Number", e);
            return null;
         }
-        if (tacoBellLocationPoint.getLongitude() < -180 || tacoBellLocationPoint.getLongitude() > 180)
-        {
+        if (tacoBellLocationPoint.getLongitude() < -180 || tacoBellLocationPoint.getLongitude() > 180) {
            logger.logWarning("Not a Valid Longitude");
            return null;
         }
@@ -65,14 +57,12 @@ public class TacoParser {
         
        
         // if (name.length < 9 || name.Substring(0, 9) != "Taco Bell")
-        if (name.length() < 9)
-        {
+        if (name.length() < 9) {
           logger.logWarning("Not a Valid location");
           return null;
         }
         
-        if (!name.contains("Taco Bell"))
-        {
+        if (!name.contains("Taco Bell")) {
           logger.logWarning("Not a Valid Taco Bell");
           return null;
         }
